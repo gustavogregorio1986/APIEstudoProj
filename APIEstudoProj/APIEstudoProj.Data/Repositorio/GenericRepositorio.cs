17df7e69453185b1509010b3a5de34873c15c6ec
guga56 <gustavogregorio086@gmail.com>
2023-03-16 15:42:06 -0300

    classe generic repositorio

---
+++ APIEstudoProj/APIEstudoProj.Data/Repositorio/GenericRepositorio.cs
@@ -0,0 +1,77 @@
+﻿using APIEstudoProj.Data.Contexto;
+using APIEstudoProj.Data.Repositorio.Interface;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace APIEstudoProj.Data.Repositorio
+{
+    public class GenericRepositori<T> : IGenericRepositorio<T>, IDisposable where T : class
+    {
+        private readonly APIEstudoProjContexto _db;
+
+        public GenericRepositori()
+        {
+            var options = new DbContextOptionsBuilder<APIEstudoProjContexto>().UseSqlServer("ConnectionStrings").Options;
+            var contexto = new APIEstudoProjContexto(options);
+        }
+
+        public void Adicionar(T entidade)
+        {
+            _db.Set<T>().Add(entidade);
+            Commit();
+        }
+
+        public void Atualizar(T entidade)
+        {
+            _db.Entry(entidade).State = EntityState.Modified;
+            Commit();
+        }
+
+        public T Buscar(params object[] Key)
+        {
+            return _db.Set<T>().Find(Key);
+        }
+
+        public void Commit()
+        {
+            _db.SaveChanges();
+        }
+
+        public void Deletar(int i)
+        {
+            var obj = Buscar(i);
+            _db.Set<T>().Remove(obj);
+            Commit();
+        }
+
+        public void Dispose()
+        {
+            if (_db != null)
+            {
+                _db.Dispose();
+            }
+
+            GC.SuppressFinalize(this);
+        }
+
+        public List<T> Obter(Expression<Func<T, bool>> predicate)
+        {
+            return _db.Set<T>().Where(predicate).ToList();
+        }
+
+        public List<T> ObterTodos()
+        {
+            return _db.Set<T>().ToList();
+        }
+
+        public T Primeiro(Expression<Func<T, bool>> predicate)
+        {
+            return _db.Set<T>().Where(predicate).FirstOrDefault();
+        }
+    }
+}
